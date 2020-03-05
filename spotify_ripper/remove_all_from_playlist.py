ec03a27526c8ee51df544465f4a917545102ab21
Sebastian Albers <seb.albers@gmail.com>
2020-03-05 22:35:59 +0100

    Some maintenance.

--- spotify_ripper/remove_all_from_playlist.py
+++ spotify_ripper/remove_all_from_playlist.py
@@ -4,8 +4,8 @@ import os
 
 scope = 'playlist-modify-public playlist-modify-private playlist-read-collaborative'
 
-client_id = os.environ["SPOTIPY_CLIENT_ID"] 
-client_secret = os.environ["SPOTIPY_CLIENT_SECRET"]
+client_id = os.environ.get('SPOTIPY_CLIENT_ID')
+client_secret = os.environ.get('SPOTIPY_CLIENT_SECRET')
 redirect_uri = 'http://www.purple.com'
 
 token = None
@@ -27,10 +27,10 @@ def get_playlist_tracks(username, playlistURI):
     global rPlaylistID
     p1, p2, p3, p4, rPlaylistID = playlistURI.split(':', 5)
 
-    global token 
+    global token
     token = util.prompt_for_user_token(username, scope)
 
-    global spotInstance 
+    global spotInstance
     spotInstance = spotipy.Spotify(auth=token)
     spotInstance.trace = False
 
